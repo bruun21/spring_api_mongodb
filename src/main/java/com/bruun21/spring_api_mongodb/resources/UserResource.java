38828379edcfaec305f18097472738d670d8b637
bruun21 <brunocrfraga@gmail.com>
2022-08-29 12:19:58 -0300

    Using DTO to return users

--- src/main/java/com/bruun21/spring_api_mongodb/resources/UserResource.java
+++ src/main/java/com/bruun21/spring_api_mongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.bruun21.spring_api_mongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bruun21.spring_api_mongodb.domain.User;
+import com.bruun21.spring_api_mongodb.dto.UserDTO;
 import com.bruun21.spring_api_mongodb.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service; // Aplicação em camadas: O controlador Rest acessa o serviço, o serviço acessa o repositório
 	
 	@RequestMapping(method=RequestMethod.GET) //GETMAPPING
-	public ResponseEntity<List<User>> findAll(){ //Objeto Spring que já encapsula toda a estrutura necessária para retono de respostas http já com possíveis cabeçalhos, erros etc.
+	public ResponseEntity<List<UserDTO>> findAll(){ //Objeto Spring que já encapsula toda a estrutura necessária para retono de respostas http já com possíveis cabeçalhos, erros etc.
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list); //.ok é um método que irá instanciar o ResponseEntity já com o código de resposta http de sucesso da operação
+		List <UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO); //.ok é um método que irá instanciar o ResponseEntity já com o código de resposta http de sucesso da operação
 		//.body Define o corpo da resposta, e no caso, com o objeto criado irá retorná-lo
 	}
 	
