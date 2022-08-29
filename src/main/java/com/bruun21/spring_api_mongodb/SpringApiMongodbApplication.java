eb96165b2d3d55e0410e32a41b031b838002a7c6
bruun21 <brunocrfraga@gmail.com>
2022-08-29 10:17:49 -0300

    Project created

---
+++ src/main/java/com/bruun21/spring_api_mongodb/SpringApiMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.bruun21.spring_api_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruun21/spring_api_mongodb/SpringApiMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruun21.spring_api_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
