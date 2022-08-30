d561b74adea91f8579f2239e2f3d151fb0a7da9f
bruun21 <brunocrfraga@gmail.com>
2022-08-30 09:41:36 -0300

    Referencing user posts

--- src/main/java/com/bruun21/spring_api_mongodb/domain/Post.java
+++ src/main/java/com/bruun21/spring_api_mongodb/domain/Post.java
@@ -4,10 +4,10 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-import javax.xml.crypto.Data;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.bruun21.spring_api_mongodb.dto.AuthorDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,12 +20,12 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	
-	private User author;
+	private AuthorDTO author;
 
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -67,10 +67,10 @@ public class Post implements Serializable {
 	}
 	
 	
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
