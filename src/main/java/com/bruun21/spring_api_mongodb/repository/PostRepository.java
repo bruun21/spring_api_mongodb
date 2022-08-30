eb8beaa3929bc2750d19e0fe7a00754931c73874
bruun21 <brunocrfraga@gmail.com>
2022-08-30 11:37:56 -0300

    Simple query with @Query

--- src/main/java/com/bruun21/spring_api_mongodb/repository/PostRepository.java
+++ src/main/java/com/bruun21/spring_api_mongodb/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.bruun21.spring_api_mongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bruun21.spring_api_mongodb.domain.Post;
 
 @Repository //repositório padrão mongo no spring Data já informando o tipo da classe de domínio que irá gerenciar, no caso "User" e o tipo de dado do id dessa classe
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
