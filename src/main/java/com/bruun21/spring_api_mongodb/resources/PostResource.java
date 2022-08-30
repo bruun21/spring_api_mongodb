5aeb2c109c7864340ad07c019b01bcd44ede0001
bruun21 <brunocrfraga@gmail.com>
2022-08-30 14:39:31 -0300

    Query with multiple criteria

--- src/main/java/com/bruun21/spring_api_mongodb/resources/PostResource.java
+++ src/main/java/com/bruun21/spring_api_mongodb/resources/PostResource.java
@@ -1,25 +1,19 @@
 package com.bruun21.spring_api_mongodb.resources;
 
-import java.net.URI;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bruun21.spring_api_mongodb.domain.Post;
-import com.bruun21.spring_api_mongodb.domain.User;
-import com.bruun21.spring_api_mongodb.dto.UserDTO;
 import com.bruun21.spring_api_mongodb.resources.util.URL;
 import com.bruun21.spring_api_mongodb.services.PostService;
-import com.bruun21.spring_api_mongodb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -40,6 +34,19 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
 		text = URL.decodeParam(text);
 		List<Post> listPost = service.findByTitle(text);
+		return ResponseEntity.ok().body(listPost);
+	}
+	
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET) 
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> listPost = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(listPost);
 	}	
 }
